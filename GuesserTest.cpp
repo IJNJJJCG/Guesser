367328dea1dc8ff2fbe3cff4ab93da62a5b1f315
IJNJJJCG <hardlifecansuck@gmail.com>
2026-04-29 11:55:47 -0700

    Modified: GuesserTest.cpp. Only this file has been committed.

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,82 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST_F(GuesserTest, TestConstructor)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+	Guesser g("secret");
+	EXPECT_TRUE(g.match("secret"));
+}
+
+TEST_F(GuesserTest, TestMatch)
+{
+	Guesser g("secret");
+	EXPECT_TRUE(g.match("secret"));
+	EXPECT_FALSE(g.match("secreT"));
+}
+
+TEST_F(GuesserTest, TestDistance)
+{
+	Guesser g("secret");
+	EXPECT_EQ(g.distance("secret"), 0);
+	EXPECT_EQ(g.distance("secreT"), 1);
+	EXPECT_EQ(g.distance("secre"), 1);
+	EXPECT_EQ(g.distance("ssecrett"), 1);
+	EXPECT_EQ(g.distance("seeecrettt"), 2);
+	EXPECT_EQ(g.distance("secrrrretttt"), 3);
+}
+
+TEST_F(GuesserTest, TestLocking)
+{
+	Guesser g("secret");
+	EXPECT_FALSE(g.match("secreT"));
+	EXPECT_FALSE(g.match("Secret"));
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_TRUE(g.match("secret"));
+}
+
+TEST_F(GuesserTest, TestTruncation)
+{
+	Guesser g("This is a very long secret that should be truncated at 32 characters");
+	EXPECT_TRUE(g.match("This is a very long secret that should be truncated at 32 characters"));
+	EXPECT_TRUE(g.match("This is a very long secret that should be truncated at 32 charact"));
+	EXPECT_FALSE(g.match("This is a very long secret that should be truncated at 32 characters!"));
+}
+
+TEST_F(GuesserTest, TestBruteForce)
+{
+	Guesser g("secret");
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("secret"));
+}
+
+TEST_F(GuesserTest, TestBruteForceWithDistance)
+{
+	Guesser g("secret");
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("st"));
+	EXPECT_FALSE(g.match("ECRE"));
+	EXPECT_TRUE(g.match("secret"));
+}
+
+TEST_F(GuesserTest, TestBruteForceWithDistanceAndRemaining)
+{
+	Guesser g("secret");
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("st"));
+	EXPECT_FALSE(g.match("ECRE"));
+	EXPECT_FALSE(g.match("secret"));
+	EXPECT_TRUE(g.match("secret"));
+}
+
+TEST_F(GuesserTest, TestBruteForceWithDistanceAndRemainingAndLocking)
+{
+	Guesser g("secret");
+	EXPECT_FALSE(g.match("sECREt"));
+	EXPECT_FALSE(g.match("st"));
+	EXPECT_FALSE(g.match("ECRE"));
+	EXPECT_FALSE(g.match("secret"));
+	EXPECT_TRUE(g.match(""));
+	EXPECT_FALSE(g.match(""));
 }
